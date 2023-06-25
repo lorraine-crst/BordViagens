cfc7cd1f745f4e00ac76719a388915ca5a5ed8fa
Lorraine Cristina <cristinalorraine424@gmail.com>
2023-06-25 10:46:55 -0300

    implementando banner

--- BordViagens/HomeTableViewHeader.swift
+++ BordViagens/HomeTableViewHeader.swift
@@ -9,12 +9,10 @@ import UIKit
 
 class HomeTableViewHeader: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+    @IBOutlet weak var tituloLabel: UILabel!
+    
+    @IBOutlet weak var HeaderView: UIView!
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
 }
